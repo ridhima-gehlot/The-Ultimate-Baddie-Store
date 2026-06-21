f50f6615456005bbc6890d13b3e7595889ce2d46
Ridhima <ridhimagehlot7@gmail.com>
2026-06-21 21:48:50 +0530

    nothing changed

--- baddie_store/settings.py
+++ baddie_store/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path, os
 from decouple import config
 import pymysql
 pymysql.install_as_MySQLdb
-import pymysql
-pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
