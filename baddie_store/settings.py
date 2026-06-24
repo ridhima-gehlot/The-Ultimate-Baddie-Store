10a7f9b6d4bcbc1aff9f32a92639fad2c4db7eb0
Ridhima <ridhimagehlot7@gmail.com>
2026-06-24 14:30:43 +0530

    Move database credentials to .env, remove .env from tracking

--- baddie_store/settings.py
+++ baddie_store/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'baddie_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD': 'ridhima',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
