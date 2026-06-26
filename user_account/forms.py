13d8648703b814a92bc2801fbb777b66e39d9473
Ridhima <ridhimagehlot7@gmail.com>
2026-06-26 17:40:36 +0530

    Made fields for first name and last name

--- user_account/forms.py
+++ user_account/forms.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 class SignupForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email=forms.EmailField(required=True)
     class Meta:
         model= User
-        fields=["username", "email", "password1", "password2"]
+        fields=["username", "first_name", "last_name" "email", "password1", "password2"]
 
