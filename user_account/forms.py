e05d1010cac3e2f14701525aec26bec6b9b72ef0
Ridhima <ridhimagehlot7@gmail.com>
2026-06-26 18:04:52 +0530

    Fix missing comma in SignupForm fields list

--- user_account/forms.py
+++ user_account/forms.py
@@ -11,5 +11,5 @@ class SignupForm(UserCreationForm):
     email=forms.EmailField(required=True)
     class Meta:
         model= User
-        fields=["username", "first_name", "last_name" "email", "password1", "password2"]
+        fields=["username", "first_name", "last_name", "email", "password1", "password2"]
 
